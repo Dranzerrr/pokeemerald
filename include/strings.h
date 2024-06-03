ade17d874fcb729058a88f942e950ced27d19100
dranzrx <168952602+dranzrx@users.noreply.github.com>
2024-06-04 00:00:31 +1000

    multichoice starter
    Added a new MultichoiceList to src\data\script_menu.h
    The strings for the multichoice were added to include\strings.h and src\strings.c

--- include/strings.h
+++ include/strings.h
@@ -3026,4 +3026,11 @@ extern const u8 gText_BoxName[];
 extern const u8 gText_PkmnsNickname[];
 extern const u8 gText_TellHimTheWords[];
 
+// Multichoice starter
+extern const u8 gText_KantoStarter[];
+extern const u8 gText_JohtoStarter[];
+extern const u8 gText_HoennStarter[];
+
+
+
 #endif // GUARD_STRINGS_H
